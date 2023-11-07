76dba7d713c5519b786c46c272e8a6a99fb03ed6
Rens Bril <Rensbril@proton.me>
2023-11-07 16:00:30 +0100

    Obsidian: Fixed  folder in LOGDIR var

--- obsidian/obsidian.sh
+++ obsidian/obsidian.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LOG_DIR="$HOME/scripts/obs_logs"
+LOG_DIR="$HOME/scripts/obsidian/obs_logs"
 LOG_FILE="$LOG_DIR/obsidian_git_sync.log"
-VAULT_DIR="$HOME/Documents/Obsidian Vault"
+VAULT_DIR="$HOME/Documents/Obsidian-Vault/"
 
 # Ensure log directory exists
 mkdir -p "$LOG_DIR"
@@ -11,25 +11,25 @@ mkdir -p "$LOG_DIR"
 cd "$VAULT_DIR" || exit 1
 
 # Add timestamp to log
-echo "===== Sync started at $(date) =====" >> "$LOG_FILE"
+echo "===== Sync started at $(date) =====" >>"$LOG_FILE"
 
 # Pull latest changes from the remote repository
-if ! git pull 2>> "$LOG_FILE"; then
-    echo "Failed to pull from remote repository." >> "$LOG_FILE"
-    exit 1
+if ! git pull 2>>"$LOG_FILE"; then
+	echo "Failed to pull from remote repository." >>"$LOG_FILE"
+	exit 1
 fi
 
 # Add all new and changed files to git
-git add . >> "$LOG_FILE" 2>&1
+git add . >>"$LOG_FILE" 2>&1
 
 # Commit the changes with a timestamp. If there's nothing to commit, it's not an error
-git commit -m "Update $(date)" >> "$LOG_FILE" 2>&1 || echo "No changes to commit" >> "$LOG_FILE"
+git commit -m "Update $(date)" >>"$LOG_FILE" 2>&1 || echo "No changes to commit" >>"$LOG_FILE"
 
 # Push the changes to your git repository
-if ! git push 2>> "$LOG_FILE"; then
-    echo "Failed to push to remote repository." >> "$LOG_FILE"
-    exit 1
+if ! git push 2>>"$LOG_FILE"; then
+	echo "Failed to push to remote repository." >>"$LOG_FILE"
+	exit 1
 fi
 
 # Add end timestamp to log
-echo "===== Sync completed at $(date) =====" >> "$LOG_FILE"
+echo "===== Sync completed at $(date) =====" >>"$LOG_FILE"
